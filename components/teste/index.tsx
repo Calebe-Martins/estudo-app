32403984415356aa08a84e6dc3a43c476bd2f578
Calebe Martins <calebe.graciano@gmail.com>
2022-12-12 11:02:43 -0300

    Utilizando a openai.com/chat com geofencing

--- components/teste/index.tsx
+++ components/teste/index.tsx
@@ -1,6 +1,6 @@
 import styles from './teste.module.css'
 
-let profile = {nome: '', email: '', telefone: '', userid: '', cpf: ''}
+let profile = {nome: '', email: '', telefone: '', id: '', cpf: ''}
 
 var teste = '{"id": "1","nome":"Calebe","email":"@gmail.com","telefone":"62","cpf":"00"}'
 
@@ -13,11 +13,11 @@ export default function Teste() {
         <>
             <p>Testando componentes dinamicos</p>
             <div>
-                <p><strong>Nome:</strong>{profile.nome}</p>
-                <p><strong>Email:</strong>{profile.email}</p>
-                <p><strong>Telefone:</strong>{profile.telefone}</p>
-                <p><strong>UserID:</strong>{profile.userid}</p>
-                <p><strong>CPF:</strong>{profile.cpf}</p>
+                <p><strong>Nome: </strong>{profile.nome}</p>
+                <p><strong>Email: </strong>{profile.email}</p>
+                <p><strong>Telefone: </strong>{profile.telefone}</p>
+                <p><strong>UserID: </strong>{profile.id}</p>
+                <p><strong>CPF: </strong>{profile.cpf}</p>
             </div>    
         </>
     )
