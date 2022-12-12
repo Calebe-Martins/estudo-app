84da8676af27cb03d703f4c17719b7e625bd7654
Calebe Martins <calebe.graciano@gmail.com>
2022-12-12 10:07:07 -0300

    Innitial Commit

---
+++ components/teste/index.tsx
@@ -0,0 +1,24 @@
+import styles from './teste.module.css'
+
+let profile = {nome: '', email: '', telefone: '', userid: '', cpf: ''}
+
+var teste = '{"id": "1","nome":"Calebe","email":"@gmail.com","telefone":"62","cpf":"00"}'
+
+// console.log(JSON.parse(teste))
+
+profile = JSON.parse(teste)
+
+export default function Teste() {
+    return (
+        <>
+            <p>Testando componentes dinamicos</p>
+            <div>
+                <p><strong>Nome:</strong>{profile.nome}</p>
+                <p><strong>Email:</strong>{profile.email}</p>
+                <p><strong>Telefone:</strong>{profile.telefone}</p>
+                <p><strong>UserID:</strong>{profile.userid}</p>
+                <p><strong>CPF:</strong>{profile.cpf}</p>
+            </div>    
+        </>
+    )
+}
